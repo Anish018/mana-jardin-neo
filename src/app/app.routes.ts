2a4ab7d709ec939e3ddadcdab742f4b0b8eff452
Anish <anish.n1809@gmail.com>
2024-03-31 18:37:26 +0530

    blog1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DiscoverJardinNeoComponent } from './discover-jardin-neo/discover-jardin-neo.component';
 import { BlogsMainComponent } from './blogs/blogs-main/blogs-main.component';
+import { Blog1Component } from './blogs/blog1/blog1.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'blog', component: BlogsMainComponent },
+    { path: 'blog1', component: Blog1Component },
     { path: 'discover-jardin-neo', component: DiscoverJardinNeoComponent }
 ];
